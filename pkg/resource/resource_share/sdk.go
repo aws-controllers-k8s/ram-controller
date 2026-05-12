24804bcb73a2821b9f4fa3da0821bb2faf099d43
fizashaikh <19280748+fizashaikh@users.noreply.github.com>
2026-05-13 05:26:24 +1200

    fix_resource_share_adoption_bug: Fix resource share adoption bug (#47)
    **Issue #, if available:** https://github.com/aws-controllers-k8s/community/issues/2819
    
    **Description of changes:**
    This PR fixes the `sdkFind()` function for ResourceShare to default the owning Account to Self if its `nil` rather than OtherAccount. 
    This fixes the issue (linked above) where a resource share exists in the aws account but the controller still can't find it because it is looking for it in OtherAccount.
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- pkg/resource/resource_share/sdk.go
+++ pkg/resource/resource_share/sdk.go
@@ -74,9 +74,10 @@ func (rm *resourceManager) sdkFind(
 	if err != nil {
 		return nil, err
 	}
-	if r.ko.Status.OwningAccountID != nil && *r.ko.Status.OwningAccountID == string(rm.awsAccountID) {
-		input.ResourceOwner = svcsdktypes.ResourceOwnerSelf
-	} else {
+
+	// defaults owner to Self, only change if we know it's owned by another account
+	input.ResourceOwner = svcsdktypes.ResourceOwnerSelf
+	if r.ko.Status.OwningAccountID != nil && *r.ko.Status.OwningAccountID != string(rm.awsAccountID) {
 		input.ResourceOwner = svcsdktypes.ResourceOwnerOtherAccounts
 	}
 	var resp *svcsdk.GetResourceSharesOutput
